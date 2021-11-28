0dc7aa4e6f98051067ae502c55ac8b59c4164574
willntt <william.yizima@global.ntt>
2021-11-28 14:25:38 -0300

    modify main.py with new function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 from module_teste import imprimindo_algo 
 
 print(imprimindo_algo())
@@ -12,4 +14,9 @@ def print_my_name_and_age(name: str, age: int) -> str:
     Returns:
         str: [description]
     """
-    return f'Hi! My name is {name} and my age is {age}'
+    return f'Hi! My name is {name} and my age is {age} years'
+
+print(print_my_name_and_age(
+                            name='will',
+                            age='50'
+                            ))
